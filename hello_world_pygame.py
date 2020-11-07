bccd9e3e136f1171ed8dbee5d2d5f2d63cb00034
판두 <pandavas89@gmail.com>
2020-11-07 10:45:18 +0900

    move logo update

--- hello_world_pygame.py
+++ hello_world_pygame.py
@@ -7,6 +7,9 @@ height = 400
 white = (255, 255, 255)
 black = (  0,   0,   0)
 fps = 30
+stride = 5
+x = 0
+y = 0
 
 pygame.init()
 
@@ -21,11 +24,23 @@ hellorect = helloworld.get_rect()
 hellorect.center = (width / 2, height / 2)
 
 while True:
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        x += stride
+    elif keys[pygame.K_a]:
+        x -= stride
+        left = True;
+    elif keys[pygame.K_w]:
+        y -= stride
+    elif keys[pygame.K_s]:
+        y += stride
     displaysurf.fill(white)
+    hellorect.center = (width/2 + x, height /2 + y)
     displaysurf.blit(helloworld, hellorect)
 
     pygame.display.update()
